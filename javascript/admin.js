9f10e8cc2bf56c122d7300423ed676cf13f3031c
Ramiro Morales Kraff <ramiromoraleskraff@gmail.com>
2022-05-28 14:21:20 -0300

    login funcionando

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,11 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const name = document.querySelector('input[type = "text"]')
-    const pass = document.querySelector('input[type = "password"]');
     const button = document.querySelector('input[type = "submit"]');
     const back = document.querySelector('#button-img');
 
     button.addEventListener('click', (event) => {
+        const name = document.querySelector('#name').value;
+        const pass = document.querySelector('#password').value;
+
         event.preventDefault();
+
+        const admin = {
+            nameOne: 'Ramless',
+            nameTwo: 'Lean',
+            password: 'admin'
+        }
+
+        name === admin.nameOne && pass === admin.password || name === admin.nameTwo && pass === admin.password?
+            window.location.href = "./admin-panel.html":
+            document.querySelector('#error-message').style.display = 'block';
 
     });
 
